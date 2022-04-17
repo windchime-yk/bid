b418b6a042bd569bd3b476ff3829c07bcf258a97
WhyK <windchimeyk@gmail.com>
2022-04-17 19:58:30 +0900

    feat: first implement

---
+++ test/convert.test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "../deps.ts";
+import { convertJsonToCsv, convertJsonToTsv } from "../util/convert.ts";
+import { testCombineArray } from "./mockdata.ts";
+
+Deno.test("convert JSON to CSV", () => {
+  assertEquals(
+    convertJsonToCsv(testCombineArray),
+    "でぃくしょなりー,Dictionary,固有名詞\nわいけー,WhyK,人名",
+  );
+});
+
+Deno.test("convert JSON to TSV", () => {
+  assertEquals(
+    convertJsonToTsv(testCombineArray),
+    "でぃくしょなりー\tDictionary\t固有名詞\nわいけー\tWhyK\t人名",
+  );
+});
