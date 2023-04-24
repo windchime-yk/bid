0e911c250331a542358b87d87974ac35885efe1a
WhyK <windchimeyk@gmail.com>
2023-04-24 20:02:10 +0900

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { ensureDir } from "https://deno.land/std@0.78.0/fs/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.110.0/testing/asserts.ts";
+export { ensureDir } from "https://deno.land/std@0.184.0/fs/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.184.0/testing/asserts.ts";
